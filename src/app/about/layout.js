e8c5e5bfee937e081f54fef236b5525dcb0cf910
Binti Bhatt <bintijbhatt@gmail.com>
2024-01-20 13:35:49 +0530

    Update layout.js

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -11,7 +11,7 @@ export default function AboutLayout({ children }) {
     <>
       <div className="bg-[url('/assets/images/hero-bg.jpg')] bg-cover">
         <section className="bg-gray-900 h-screen py-10 sm:py-1 px-4 md:px-10 md:py-4 lg:px-8 lg:py-10 xl:py-20 bg-opacity-70">
-          <div className="container mx-auto mt-16 lg:mt-8 xl:mt-10 md:mt-4 sm:mt-8 border-gray-400 border-2 rounded-3xl">
+          <div className="container mx-auto mt-16 lg:mt-4 md:mt-4 sm:mt-8 border-gray-400 border-2 rounded-3xl">
             <Navbar />
             {children}
             <Footer />
